25091b4fda3a6734af800729af93cba4dd248b4e
mk36fyvy <mikreck@hotmail.com>
2020-02-18 13:59:27 +0100

    disgusting fix in scoring.py. Will be fixed correctly.

--- multivitamin/utils/scoring.py
+++ multivitamin/utils/scoring.py
@@ -128,7 +128,7 @@ class Scoring():
                 graph_score += node_score
             gap_node_amount = self.large_graph_nodes_len + self.small_graph_nodes_len - (2 * mapped)
             graph_score += self.gap_score*gap_node_amount * node_label_len * mapping_label_len
-            self.res_scores[tuple(sorted(res.items()))] = int(graph_score/(node_label_len + mapping_label_len))
+            self.res_scores[tuple(sorted(res.items()))] = int(graph_score/(node_label_len + mapping_label_len + 0.1))
 
 
     def get_best_result( self ):
